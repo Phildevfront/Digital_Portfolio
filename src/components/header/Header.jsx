be3d6f0e815e5aa9e4c69cbbe5f5cef7af276ac2
Philippe Baurens <p.baurens@mazarine.com>
2022-10-30 00:03:46 +0200

    About header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 	return (
 	<header className="header">
 		<nav className="nav_container">
-			<a href="index.html" className="nav__log">P.BAURENS</a>
+			<a href="index.html" className="nav__logo">p.baurens</a>
 
 			<div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
 				<ul className="nav__list grid">
